b6629a7b82a9ef5507526264b70ac0fe08699ec3
lcpojr <lcpojr@hotmail.com>
2017-04-05 23:30:20 -0300

    Register Inicial

--- estacaocedro/urls.py
+++ estacaocedro/urls.py
@@ -4,4 +4,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('apps.website.urls', namespace='website')),
+    url(r'^painel/', include('apps.painel.urls', namespace='painel')),
 ]
