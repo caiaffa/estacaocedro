71dae8050c8b566eabe030e198bbb2414a8a0d3e
Luís Felipe <lfa.luisfelipe@gmail.com>
2017-04-06 09:07:46 -0300

    Form contato

--- apps/website/models.py
+++ apps/website/models.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
-# Create your models here.
+class Contato(models.Model):
+    nome = models.CharField(max_length=100, verbose_name='Nome')
+    email = models.EmailField(max_length=100,  blank=True, null=True, verbose_name='E-mail')
+    telefone = models.CharField(max_length=100,  blank=True, null=True, verbose_name='Telefone')
+    descricao = models.CharField(max_length=255,  blank=True, null=True, verbose_name='Descrição')
+    is_visualizada = models.BooleanField(default=False, verbose_name='Visualizada ?')
+    class Meta:
+        verbose_name_plural = 'Contatos'
+
+    def __str__(self):
+        return self.nome
