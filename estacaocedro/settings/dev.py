02c7cf17e278e76844d9186f036c31caaf16b4d6
Luís Felipe <lfa.luisfelipe@gmail.com>
2017-04-05 21:31:09 -0300

    Ajustes Inicias

---
+++ estacaocedro/settings/dev.py
@@ -0,0 +1,17 @@
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'estacaocedro',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': ''
+    }
+}
+
+#INSTALLED_APPS += ('debug_toolbar',)
+
+#INTERNAL_IPS = ('127.0.0.1')
+
+
