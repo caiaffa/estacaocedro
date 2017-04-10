384022fd131c564f8ff026feaf12336e36047e3c
lcpojr <lcpojr@hotmail.com>
2017-04-10 10:19:42 -0300

    Publicação resumo e img size

--- apps/painel/forms.py
+++ apps/painel/forms.py
@@ -4,17 +4,20 @@ from .models import Publicacao
 class PublicacaoForm(forms.ModelForm):
 	class Meta:
 		model = Publicacao
-		fields = ['titulo', 'imagem', 'conteudo']
+		fields = ['titulo', 'resumo', 'imagem', 'conteudo']
 
 	def __init__(self, *args, **kwargs):
 		super(PublicacaoForm, self).__init__(*args, **kwargs)
 		self.fields['titulo'].widget.attrs['class'] = 'form-control'
+		self.fields['resumo'].widget.attrs['class'] = 'form-control'
 		self.fields['imagem'].widget.attrs['class'] = 'form-control'
 		self.fields['conteudo'].widget.attrs['class'] = 'form-control summernote'
 
 		self.fields['titulo'].widget.attrs['placeholder'] = 'Título da publicação'
+		self.fields['resumo'].widget.attrs['placeholder'] = 'Breve resumo da publicação'
 		self.fields['imagem'].widget.attrs['placeholder'] = 'Imagem de capa da publicação'
 		self.fields['conteudo'].widget.attrs['placeholder'] = 'Conteúdo da publicação'
 
 		self.fields['titulo'].widget.attrs['data-validation'] = '[NOTEMPTY]'
+		self.fields['resumo'].widget.attrs['data-validation'] = '[NOTEMPTY]'
 		self.fields['conteudo'].widget.attrs['data-validation'] = '[NOTEMPTY]'
