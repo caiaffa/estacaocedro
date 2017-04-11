b0b651d5c0fa5498e39424f5592c604484ba6d97
lcpojr <lcpojr@hotmail.com>
2017-04-11 12:49:57 -0300

    Create prod.py

---
+++ estacaocedro/settings/prod.py
@@ -0,0 +1,20 @@
+from .base import *
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'estacaocedro',
+        'USER': 'root',
+        'PASSWORD': 'i3l1a2b3c#',
+        'HOST': ''
+    }
+}
+
+INSTALLED_APPS += (
+	#'debug_toolbar',
+)
+
+#INTERNAL_IPS = ('127.0.0.1')
+
+ALLOWED_HOSTS = ('*')
