38184e3550bafcf52913c685b1747e067171fe55
lcpojr <lcpojr@hotmail.com>
2017-04-24 12:46:57 -0300

    Update prod.py

--- estacaocedro/settings/prod.py
+++ estacaocedro/settings/prod.py
@@ -14,3 +14,42 @@ DATABASES = {
 INTERNAL_IPS = ('127.0.0.1')
 
 ALLOWED_HOSTS = ('*')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+        'error_file': {
+            'class': 'logging.FileHandler',
+            'formatter': 'generic',
+            'filename': '/home/i3lsite/websites/estacaocedro/gunicorn.error.log',
+        },
+        'access_file': {
+            'class': 'logging.FileHandler',
+            'formatter': 'generic',
+            'filename': '/home/i3lsite/websites/estacaocedro/gunicorn.access.log',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'gunicorn.error': {
+            'level': 'INFO',
+            'handlers': ['error_file'],
+            'propagate': True,
+        },
+        'gunicorn.access': {
+            'level': 'INFO',
+            'handlers': ['access_file'],
+            'propagate': False,
+        },
+    },
+}
