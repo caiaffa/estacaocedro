3b3c729a48e353952127da1eb72f23a794b75f97
lcpojr <lcpojr@hotmail.com>
2017-04-27 13:01:31 -0300

    Fix sitemaps

--- estacaocedro/sitemaps.py
+++ estacaocedro/sitemaps.py
@@ -22,14 +22,14 @@ class NoticiaSitemap(Sitemap):
 		urls = []
 		publicacoes = Publicacao.objects.all()
 		for publicacao in publicacoes:
-			urls.append({'name':'website:noticia', 'pk':publicacao.pk})
+			urls.append({'name':'website:noticia', 'pk':publicacao.pk, 'data': publicacao.data})
 		return urls
 
 	def location(self, url):
-		return reverse(url.name, kwargs={'pk':url.pk})
+		return reverse(url['name'], kwargs={'pk':url['pk']})
 
 	def lastmod(self, url):
-		return reverse(url.data)
+		return url['data']
 
 
 class GaleriaSitemap(Sitemap):
@@ -40,11 +40,11 @@ class GaleriaSitemap(Sitemap):
 		urls = []
 		albuns = Album.objects.all()
 		for album in albuns:
-			urls.append({'name':'website:galeria', 'pk':album.pk})
+			urls.append({'name':'website:galeria', 'pk':album.pk, 'data': album.data})
 		return urls
 
 	def location(self, url):
-		return reverse(url.name, kwargs={'pk':url.pk})
+		return reverse(url['name'], kwargs={'pk':url['pk']})
 
 	def lastmod(self, url):
-		return reverse(url.data)
+		return url['data']
